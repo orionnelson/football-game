6da5646aa47be44bc0d08a649774824c1166e809
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-04 17:36:29 -0400

    Update TestViewChanges.java

--- src/test/TestViewChanges.java
+++ src/test/TestViewChanges.java
@@ -1,4 +1,8 @@
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestViewChanges {
-
+	@Test
+    public void testHelloWorld() {
+        assertEquals("hello world", MessageBuilder.getHelloWorld());
+    }
 }
