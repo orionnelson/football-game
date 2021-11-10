50561136fe5fec0c2b40a31cd1c4133378970041
denjcodes <d2j666@hotmail.com>
2021-11-09 21:54:48 -0500

    Updated docs

--- src/test/TestMenuListener.java
+++ src/test/TestMenuListener.java
@@ -14,10 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Test menu actions
- */
-public class TestMenuListener {
 import java.awt.AWTException;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -29,110 +25,159 @@ import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.border.Border;
 
+/**
+ * Test menu actions
+ */
+
 public class TestMenuListener{
+
+
 	
+	/**
+	 * Frame for testing Game
+	 */
+    public static JFrame gameFrame = new JFrame("Mini Soccer");
+    
+    /**
+     * Game panel used for testing Game
+     */
+	public static GamePanel gamePanel = new GamePanel();
 	
-	  public static JFrame gameFrame = new JFrame("Mini Soccer");
-		public static GamePanel gamePanel = new GamePanel();
-		public static GameListener gl = new GameListener(gamePanel);
-		public static MenubarListener menubarListener = new MenubarListener(gamePanel);
-		public static GameMenuBar gameMenuBar = new GameMenuBar(menubarListener);
-		SoccerGame sg = gamePanel.getGame();
-		public void setupGame(Point sp) {
-			gameFrame.add(gamePanel);
-			gameFrame.addKeyListener(gl);
-			gameFrame.setJMenuBar(gameMenuBar);
-			gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			gameFrame.setSize(600, 600);
-			gameFrame.setLocationRelativeTo(null);
-			gameFrame.setResizable(false);
-			gameFrame.setVisible(true);
-			sg.getActivePlayer().setPlayerPosition(sp);
-			genEvent(KeyEvent.VK_R);
-			 
-		}
+	/**
+	 * Listens for input from player and used for testing
+	 */
+	public static GameListener gl = new GameListener(gamePanel);
 	
+	/**
+	 * Listens for input from player via menu bar and used for testing
+	 */
+	public static MenubarListener menubarListener = new MenubarListener(gamePanel);
+	
+	/**
+	 * Menu bar used to listen for game bar events
+	 */
+	public static GameMenuBar gameMenuBar = new GameMenuBar(menubarListener);
+	
+	/**
+	 * Main game panel used to setup the game
+	 */
+	SoccerGame sg = gamePanel.getGame();
+	
+	/**
+	 * Setup game using specified parameters and starts the game via Resume (R)
+	 * @param sp Sets the start location of active player
+	 */	
+	public void setupGame(Point sp) {
+		gameFrame.add(gamePanel);
+		gameFrame.addKeyListener(gl);
+		gameFrame.setJMenuBar(gameMenuBar);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setSize(600, 600);
+		gameFrame.setLocationRelativeTo(null);
+		gameFrame.setResizable(false);
+		gameFrame.setVisible(true);
+		sg.getActivePlayer().setPlayerPosition(sp);
+		genEvent(KeyEvent.VK_R);
+
+	}
+
+	
+	/**
+	 * @param called Uses keyboard events to simulate player movement
+	 * @return returns a new point if the game is not paused and not over
+	 */
 	public Point genEvent(int called) {
 		Point pos1  =  sg.getActivePlayer().getPlayerPosition();
 		try {
 			Robot r = new Robot();
-			 r.setAutoWaitForIdle(true);
-		     r.delay(1500);
-		     r.waitForIdle();
+			r.setAutoWaitForIdle(true);
+			r.delay(1500);
+			r.waitForIdle();
 			r.keyPress(called);r.keyRelease(called);
 			r.keyPress(called);r.keyRelease(called);
-			
+
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Point pos2 = new Point();
 		if (!sg.isPaused() && !sg.isOver()) {
-		
-					pos2 = sg.getActivePlayer().getPlayerPosition();
-				
-	}
+
+			pos2 = sg.getActivePlayer().getPlayerPosition();
+
+		}
 		int x  = ( (int)pos2.getX() - (int)pos1.getX());
 		int y = (int) (pos2.getY()-pos1.getY());
-	return new Point(x,y);
+		return new Point(x,y);
 	}
-	
-	
+
+
+	/**
+	 * Simulates a person clicking 
+	 * @param x X coordinate of the person clicking
+	 * @param y Y coorindate of the person clicking
+	 */
 	public void genRobotClicker(int x, int y) {
-			Robot r;
-			try {
-				 r = new Robot();
-				 r.mouseMove(x,y);
-				 r.mousePress(InputEvent.BUTTON2_DOWN_MASK);
-				 try{Thread.sleep(250);}catch(InterruptedException e){}
-				 r.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
-			} catch (AWTException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
+		Robot r;
+		try {
+			r = new Robot();
+			r.mouseMove(x,y);
+			r.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+			try{Thread.sleep(250);}catch(InterruptedException e){}
+			r.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
+		} catch (AWTException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 	}
-	
-	
-	
-public boolean isWindows(){
 
-	return new File("C:\\windows").exists();
-}
 
-	
-        @Test
+	/**
+	 * Needed to test if running on Windows
+	 * @return True if platform is Windows, False therwise
+	 */
+	public boolean isWindows(){
+
+		return new File("C:\\windows").exists();
+	}
+
+
+	/**
+	 * Checks that the Reset is working as intended
+	 */
+	@Test
 	@DisplayName("Make sure That Menubar Reset Works as Intended")
-         public void testMenuReset() {
+	public void testMenuReset() {
 		setupGame(new Point(265,305));
 		SoccerBall.getSoccerBall().setPosition(sg.getActivePlayer().getPlayerPosition());
 		Point frame = gameFrame.getLocationOnScreen();
 		genEvent(KeyEvent.VK_UP);
 		genEvent(KeyEvent.VK_DOWN);
-        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+45);
-        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+55);
-        //Somthing is broken
-        assertTrue(SoccerBall.getSoccerBall().getPosition().getX()==255 && SoccerBall.getSoccerBall().getPosition().getY()==360);
-    }
-	
-	
-//	@Test
-//	@DisplayName("Make sure That Menu Reset Works as Intended")
-//	public void testMenuQuit() {
-//		setupGame(new Point(265,305));
-//		SoccerBall.getSoccerBall().setPosition(sg.getActivePlayer().getPlayerPosition());
-//		Point frame = gameFrame.getLocationOnScreen();
-//		genEvent(KeyEvent.VK_UP);
-//        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+45);
-//        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+85);
-//        genEvent(KeyEvent.VK_DOWN);
-//  
-//        //Somthing is broken
-//        assertFalse(gameFrame.isDisplayable());
-//        
-//    }
-	
-	
+		genRobotClicker((int)frame.getX()+25,(int)frame.getY()+45);
+		genRobotClicker((int)frame.getX()+25,(int)frame.getY()+55);
+		//Somthing is broken
+		assertTrue(SoccerBall.getSoccerBall().getPosition().getX()==255 && SoccerBall.getSoccerBall().getPosition().getY()==360);
+	}
+
+
+	//	@Test
+	//	@DisplayName("Make sure That Menu Reset Works as Intended")
+	//	public void testMenuQuit() {
+	//		setupGame(new Point(265,305));
+	//		SoccerBall.getSoccerBall().setPosition(sg.getActivePlayer().getPlayerPosition());
+	//		Point frame = gameFrame.getLocationOnScreen();
+	//		genEvent(KeyEvent.VK_UP);
+	//        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+45);
+	//        genRobotClicker((int)frame.getX()+25,(int)frame.getY()+85);
+	//        genEvent(KeyEvent.VK_DOWN);
+	//  
+	//        //Somthing is broken
+	//        assertFalse(gameFrame.isDisplayable());
+	//        
+	//    }
+
+
 	/**
 	 * Generic test case for testing
 	 */
@@ -141,33 +186,33 @@ public boolean isWindows(){
 	public void testPauseResume() {
 		boolean opaused;
 		boolean paused;
-		
+
 		setupGame(new Point(265,305));
 		SoccerBall.getSoccerBall().setPosition(sg.getActivePlayer().getPlayerPosition());
 		Point frame = gameFrame.getLocationOnScreen();
 		genEvent(KeyEvent.VK_UP);
-		 genEvent(KeyEvent.VK_DOWN);
-        genRobotClicker((int)frame.getX()+80,(int)frame.getY()+45);
-        genRobotClicker((int)frame.getX()+80,(int)frame.getY()+55);
-        opaused = sg.isPaused(); //true false
+		genEvent(KeyEvent.VK_DOWN);
+		genRobotClicker((int)frame.getX()+80,(int)frame.getY()+45);
+		genRobotClicker((int)frame.getX()+80,(int)frame.getY()+55);
+		opaused = sg.isPaused(); //true false
 		genEvent(KeyEvent.VK_UP);
-		 genEvent(KeyEvent.VK_DOWN);
-        genRobotClicker((int)frame.getX()+80,(int)frame.getY()+45);
-        genRobotClicker((int)frame.getX()+80,(int)frame.getY()+85);
-        //false
-        genEvent(KeyEvent.VK_UP);
-	genEvent(KeyEvent.VK_DOWN);
-		
-	paused =  sg.isPaused();
-        
-	System.out.println(opaused);
-	System.out.println(paused);
-	if (isWindows()){
-	assertTrue(!opaused && !paused);
-	}else{
-        assertTrue(!opaused && !paused);
+		genEvent(KeyEvent.VK_DOWN);
+		genRobotClicker((int)frame.getX()+80,(int)frame.getY()+45);
+		genRobotClicker((int)frame.getX()+80,(int)frame.getY()+85);
+		//false
+		genEvent(KeyEvent.VK_UP);
+		genEvent(KeyEvent.VK_DOWN);
+
+		paused =  sg.isPaused();
+
+		System.out.println(opaused);
+		System.out.println(paused);
+		if (isWindows()){
+			assertTrue(!opaused && !paused);
+		}else{
+			assertTrue(!opaused && !paused);
+		}
+
 	}
-        
-    }
-	
+
 }
--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -1,11 +1,4 @@
 package test;
-import org.junit.jupiter.api.Test;
-
-import controller.GameListener;
-import controller.MenubarListener;
-import model.SoccerBall;
-import model.SoccerGame;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,7 +11,12 @@ import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
 import org.junit.jupiter.api.DisplayName;
-import test.TestListener;
+import org.junit.jupiter.api.Test;
+
+import controller.GameListener;
+import controller.MenubarListener;
+import model.SoccerBall;
+import model.SoccerGame;
 import view.GameMenuBar;
 import view.GamePanel;
 
@@ -26,38 +24,38 @@ import view.GamePanel;
  *	Class to test models and in game events
  */
 public class TestModels{
-	
-	
+
+
 	/**
 	 * Frame for testing Game
 	 */
-    public static JFrame gameFrame = new JFrame("Mini Soccer");
-    
-    /**
-     * Game panel used for testing Game
-     */
+	public static JFrame gameFrame = new JFrame("Mini Soccer");
+
+	/**
+	 * Game panel used for testing Game
+	 */
 	public static GamePanel gamePanel = new GamePanel();
-	
+
 	/**
 	 * Listens for input from player and used for testing
 	 */
 	public static GameListener gl = new GameListener(gamePanel);
-	
+
 	/**
 	 * Listens for input from player via menu bar and used for testing
 	 */
 	public static MenubarListener menubarListener = new MenubarListener(gamePanel);
-	
+
 	/**
 	 * Menu bar used to listen for game bar events
 	 */
 	public static GameMenuBar gameMenuBar = new GameMenuBar(menubarListener);
-	
+
 	/**
 	 * Main game panel used to setup the game
 	 */
 	SoccerGame sg = gamePanel.getGame();
-	
+
 	/**
 	 * Setup game using specified parameters and starts the game via Resume (R)
 	 * @param sp Sets the start location of active player
@@ -73,9 +71,9 @@ public class TestModels{
 		gameFrame.setVisible(true);
 		sg.getActivePlayer().setPlayerPosition(sp);
 		genEvent(KeyEvent.VK_R);
-		 
+
 	}
-	
+
 	/**
 	 * @param called Uses keyboard events to simulate player movement
 	 * @return returns a new point if the game is not paused and not over
@@ -85,27 +83,27 @@ public class TestModels{
 		try {
 			Robot r = new Robot();
 			r.setAutoWaitForIdle(true);
-		    r.delay(1500);
-		    r.waitForIdle();
+			r.delay(1500);
+			r.waitForIdle();
 			r.keyPress(called);r.keyRelease(called);
 			r.keyPress(called);r.keyRelease(called);
-			
+
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Point pos2 = new Point();
 		if (!sg.isPaused() && !sg.isOver()) {
-		
-					pos2 = sg.getActivePlayer().getPlayerPosition();
-				
-	}
+
+			pos2 = sg.getActivePlayer().getPlayerPosition();
+
+		}
 		int x  = ( (int)pos2.getX() - (int)pos1.getX());
 		int y = (int) (pos2.getY()-pos1.getY());
-	return new Point(x,y);
+		return new Point(x,y);
 	}
-	
-	
+
+
 	/**
 	 * Test if the game defender is active or null
 	 */
@@ -115,10 +113,10 @@ public class TestModels{
 		setupGame(new Point(265,305));
 		//Teleports the ball to the player 
 		assertEquals(sg.getGamePlayers().get("Defence"),null);
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Test if the player is active
 	 */
@@ -129,19 +127,8 @@ public class TestModels{
 		//Teleports the ball to the player 
 		assertNotEquals(sg.getGamePlayers().get("Striker"),null);
 	}
-	
-	
-	/**
-	 * Test if Goal keeper is active
-	 */
-	@Test
-	@DisplayName("Test If Get Goalie is working")
-	public void getGoalie() {
-		setupGame(new Point(265,305));
-		//Teleports the ball to the player 
-		assertNotEquals(sg.getGamePlayers().get("Goalkeeper"),null);
-	}	
-	
+
+
 	/**
 	 * Test if Goal keeper is not active
 	 */
@@ -152,7 +139,7 @@ public class TestModels{
 		//Teleports the ball to the player 
 		assertTrue(sg.getGamePlayers().get("Goalkeeper").getPlayerStatistics()>=0);
 	}
-	
+
 	/**
 	 * Test if player is able to score
 	 */
@@ -163,55 +150,47 @@ public class TestModels{
 		//Teleports the ball to the player 
 		assertTrue(sg.getGamePlayers().get("Striker").getPlayerStatistics()>=0);
 	}
-		@Test
-		@DisplayName("Test Goalie is not null in getGamePlayers")
-		public void getGoalie() {
-			setupGame(new Point(265,305));
-			//Teleports the ball to the player 
-			assertNotEquals(sg.getGamePlayers().get("Goalkeeper"),null);
-		}	
-		
-		@Test
-		@DisplayName("Test If Get Goalie is working")
-		public void testGoalieStats() {
-			setupGame(new Point(265,305));
-			//Teleports the ball to the player 
-			assertEquals(sg.getGamePlayers().get("Goalkeeper").getPlayerStatistics()>=0,true);
-		}
-		
-		
-		@Test
-		@DisplayName("Test If Get Striker is working")
-		public void testStrikerStats() {
-			setupGame(new Point(265,305));
-			//Teleports the ball to the player 
-			assertEquals(sg.getGamePlayers().get("Striker").getPlayerStatistics()>=0,true);
-		}
-		
-	
-	        @Test
-		@DisplayName("Test Entitys To String")
-		public void testToString(){
-			setupGame(new Point(265,305));
-			assertTrue(sg.getGamePlayers().get("Striker").toString()!=null&&sg.getGamePlayers().get("Goalkeeper").toString()!=null);
-		}
-	
-		@Test 
-		@DisplayName("Test On New Start Game Goals can be scored")
-		public void testnewGame() {
-			setupGame(new Point(265,305));
-			sg.setGoal(99);
-			boolean goalworks = sg.getGoal().equals(99);
-			sg.getGamePlayers().sort();
-			genEvent(KeyEvent.VK_UP);
-			genEvent(KeyEvent.VK_N);
-			SoccerBall.getSoccerBall().setPosition(new Point(181,11));
-			int cgl = sg.getActivePlayer().getPlayerStatistics();
-			System.out.println(sg.getActivePlayer().getPlayerStatistics());
-			//This should be one somthing is broken
-			boolean gsonnewgame  = (sg.getActivePlayer().getPlayerStatistics()-cgl)==(0);
-			assertTrue(goalworks&&gsonnewgame);
-		}
-	
-	
+
+	/**
+	 * Test if GoalKeeper is available in game players
+	 */
+	@Test
+	@DisplayName("Test Goalie is not null in getGamePlayers")
+	public void getGoalie() {
+		setupGame(new Point(265,305));
+		//Teleports the ball to the player 
+		assertNotEquals(sg.getGamePlayers().get("Goalkeeper"),null);
+	}	
+
+	/**
+	 *  Checks that the game player names are correctly converted 
+	 */
+	@Test
+	@DisplayName("Test Entitys To String")
+	public void testToString(){
+		setupGame(new Point(265,305));
+		assertTrue(sg.getGamePlayers().get("Striker").toString()!=null&&sg.getGamePlayers().get("Goalkeeper").toString()!=null);
+	}
+
+	/**
+	 * Test On New Start Game Goals can be scored
+	 */
+	@Test 
+	@DisplayName("Test On New Start Game Goals can be scored")
+	public void testnewGame() {
+		setupGame(new Point(265,305));
+		sg.setGoal(99);
+		boolean goalworks = sg.getGoal().equals(99);
+		sg.getGamePlayers().sort();
+		genEvent(KeyEvent.VK_UP);
+		genEvent(KeyEvent.VK_N);
+		SoccerBall.getSoccerBall().setPosition(new Point(181,11));
+		int cgl = sg.getActivePlayer().getPlayerStatistics();
+		System.out.println(sg.getActivePlayer().getPlayerStatistics());
+		//This should be one something is broken
+		boolean gsonnewgame  = (sg.getActivePlayer().getPlayerStatistics()-cgl)==(0);
+		assertTrue(goalworks&&gsonnewgame);
+	}
+
+
 }
